b2f2d9ed601bab117ad157e880038bb467f04add
Jerod Herzog <jdhrzg@gmail.com>
2025-03-18 07:45:24 -0500

    Complete Step 1 - Add links to header

--- coder/project/reactlab/src/components/Header.js
+++ coder/project/reactlab/src/components/Header.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import {
+import
+{
   faGithub,
   faLinkedin,
   faMedium,
@@ -12,31 +13,26 @@ import { Box, HStack } from "@chakra-ui/react";
 const socials = [
   {
     icon: faEnvelope,
-    url: "mailto: hello@example.com",
+    url: "mailto: jdhrzg@gmail.com",
   },
   {
     icon: faGithub,
-    url: "https://github.com",
+    url: "https://github.com/jdhrzg",
   },
   {
     icon: faLinkedin,
-    url: "https://www.linkedin.com",
-  },
-  {
-    icon: faMedium,
-    url: "https://medium.com",
-  },
-  {
-    icon: faStackOverflow,
-    url: "https://stackoverflow.com",
+    url: "https://www.linkedin.com/in/jerod-herzog",
   },
 ];
 
-const Header = () => {
-  const handleClick = (anchor) => () => {
+const Header = () =>
+{
+  const handleClick = (anchor) => () =>
+  {
     const id = `${anchor}-section`;
     const element = document.getElementById(id);
-    if (element) {
+    if (element)
+    {
       element.scrollIntoView({
         behavior: "smooth",
         block: "start",
@@ -64,11 +60,18 @@ const Header = () => {
           alignItems="center"
         >
           <nav>
-            {/* Add social media links based on the `socials` data */}
+            <HStack spacing={4}>
+              {socials.map((social, index) => (
+                <a key={index} href={social.url} target="_blank">
+                  <FontAwesomeIcon icon={social.icon} size="2x" />
+                </a>
+              ))}
+            </HStack>
           </nav>
           <nav>
             <HStack spacing={8}>
-              {/* Add links to Projects and Contact me section */}
+              <a href="#projects-section" onClick={handleClick("projects")}>Projects</a>
+              <a href="#contactme-section" onClick={handleClick("contactme")}>Contact Me</a>
             </HStack>
           </nav>
         </HStack>
